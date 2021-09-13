8460fc9e01112fa69f23ee715e1d10ac3fe20f68
Casey Reagan <caseyreagan0@gmail.com>
2021-09-13 13:38:36 +0800

    Add more comments and change iters num

--- chap4/4.5.2_train_neuralnet.py
+++ chap4/4.5.2_train_neuralnet.py
@@ -9,15 +9,16 @@ from gradient_2_layers_net import TwolayerNet
 train_loss_list = []
 
 #超参数
-iters_num = 10000
+#iters_num = 10000		# 梯度更新的次数，此处为10000次
+iters_num = 100
 train_size = x_train.shape[0]
-batch_size = 100
+batch_size = 100 		# mini batch的大小为100 ，这样相当于每次要从60000个训练数据中随机取出100个
 learning_rate = 0.1
 network = TwolayerNet(input_size=784, hidden_size=50,output_size=10)
 
 for i in range(iters_num):
 	#	获取mini-batch
-	batch_mask = np.random.choice(train_size, batch_size)
+	batch_mask = np.random.choice(train_size, batch_size)	# 从train_size这么多个数据（60000）中随机抽取batch_size这么多个数据(100)个
 	x_batch = x_train[batch_mask]
 	t_batch = t_train[batch_mask]
 
@@ -25,9 +26,10 @@ for i in range(iters_num):
 	grad = network.numerical_gradient(x_batch, t_batch)
 	#	grad = network.gradient(x_batch,t_batch)	# 高速版
 
+	# 梯度下降，因为这里mini batch是随机选的，所以是SGD方法，随机梯度下降法
 	for key in ('W1', 'b1', 'W2', 'b2'):
 		network.params[key] -= learning_rate * grad[key]
 
-	# 记录学习过程
+	# 记录学习过程，记录每一次的loss
 	loss = network.loss(x_batch, t_batch)
 	train_loss_list.append(loss)
