cd181dc4398ac3a14e2babc2061dea861456b9f8
Casey Reagan <caseyreagan0@gmail.com>
2021-09-21 23:53:20 +0800

    finish buy apple orange procedure by 2 layer classes

---
+++ chap5/ch542buy_apple_orange.py
@@ -0,0 +1,32 @@
+import sys, os
+import numpy as np
+from ch541mullayer import MulLayer
+from ch542AddLayer import AddLayer
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+#backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+
+print("price is:",price)
+print(dapple_num, dapple, dorange, dorange_num, dtax)
