912286bfe8fe629db8fa547c5c5e8900518310de
Casey Reagan <caseyreagan0@gmail.com>
2021-09-24 14:55:54 +0800

    finish the Affine layer

---
+++ chap5/ch562Affine_layer.py
@@ -0,0 +1,25 @@
+import sys ,os
+sys.path.append(os.pardir)
+import numpy as np
+
+class Affine(object):
+	"""docstring for Affine"""
+	def __init__(self, W, b):
+		self.W = W
+		self.b = b
+		self.x = None
+		self.dW = None
+		self.db = None
+
+	def forward(self, x):
+		self.x = x
+		out = np.dot(x, self.W) + self.b
+
+		return out
+
+	def backward(self, dout):
+		dx = np.dot(dout, self.W.T)
+		self.dW = np.dot(self.x.T, dout)
+		self.db = np.sum(dout, axis=0)
+
+		return dx
