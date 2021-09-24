1bd523a19dd3c09e125ff0da7120c9bba2ec5e4e
Casey Reagan <caseyreagan0@gmail.com>
2021-09-24 18:23:11 +0800

    add the loss function and accuracy function

--- chap5/ch571two_layer_net.py
+++ chap5/ch571two_layer_net.py
@@ -28,5 +28,16 @@ class TwoLayerNet(object):
 		for layer in self.layers.values():
 			x = layer.forward(x)
 
+	def loss(self, x, t):
+		y = self.predict(x)
+		return self.lastLayer.forward(y, t)
 
-		
+	def accuracy(self, x, t):
+		y = self.predict(x)
+		y = np.argmax(y, axis=1)
+		if t.ndim != 1 : t = np.argmax(t, axis=1)
+		accuracy = np.sum(y == t) / float(x.shape[0])
+
+		return accuracy
+
+	
