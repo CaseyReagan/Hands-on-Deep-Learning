4c35a90c85f9c346657dcc97bd560e6fd3b8575c
Casey Reagan <caseyreagan0@gmail.com>
2021-09-24 17:58:34 +0800

    finish the two layer net work by organize them in layers

---
+++ chap5/ch571two_layer_net.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+
+class TwoLayerNet(object):
+	"""docstring for TwoLayerNet"""
+	def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+		# 初始化权重
+		self.params = {}
+		self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+		self.params['b1'] = np.zeros(hidden_size)
+		self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+		self.params['b2'] = np.zeros(output_size)
+
+		# 生成层
+		self.layers = OrderedDict()
+		self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+		self.layers['Relu1'] = Relu()
+		self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+		self.lastLayer = SoftmaxWithLoss()
+
+
+	def predict(self, x):
+		for layer in self.layers.values():
+			x = layer.forward(x)
+
+
+		
