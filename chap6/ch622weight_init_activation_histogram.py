33cc1280c01715e63a53c6c88b6044ef99360be2
Casey Reagan <caseyreagan0@gmail.com>
2021-09-28 23:36:00 +0800

    gradient vanishing

---
+++ chap6/ch622weight_init_activation_histogram.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+	return 1/(1 + np.exp(-x))
+
+x = np.random.randn(1000,100) # 1000个数据 高斯分布
+node_num = 100					# 各隐藏层节点的数量
+hidden_layer_size = 5			# 5层隐藏层
+activations = {}				# 激活值的结果存在这里
+
+for i in range(hidden_layer_size):
+	if i != 0:
+		x = activations[i-1]
+
+	w = np.random.randn(node_num, node_num) * 1 	#标准差为1的高斯分布
+
+	z = np.dot(x, w)
+	a = sigmoid(z)
+	activations[i] = a
+
+for i, a in activations.items():
+	plt.subplot(1, len(activations), i+1)
+	plt.title(str(i+1) + "-layer")
+	plt.hist(a.flatten(), 30, range=(0,1))
+plt.show()
