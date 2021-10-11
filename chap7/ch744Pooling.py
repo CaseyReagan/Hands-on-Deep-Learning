cb7e67251c7649cb83f603468888a0bc8d9b9883
Casey Reagan <caseyreagan0@gmail.com>
2021-10-11 15:31:22 +0800

    update comments about the forward functions

--- chap7/ch744Pooling.py
+++ chap7/ch744Pooling.py
@@ -13,17 +13,17 @@ class Pooling(object):
 
 	def forward(self, x):
 		N, C, H, W = x.shape
-		out_h = int(1 + (H - self.pool_h) / self.stride)
+		out_h = int(1 + (H - self.pool_h) / self.stride)	## 首先要计算出不转换原始数据的话池化之后的高和宽
 		out_w = int(1 + (W - self.pool_w) / self.stride)
 
 		## 展开（1）
-		col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
-		col = col.reshape(-1, self.pool_h * self.pool_w)
+		col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)	## 根据池化核的大小展开
+		col = col.reshape(-1, self.pool_h * self.pool_w)	## 把展开数据改变为h*w这么多行，-1维度自动计算出需要多少列
 
 		## 最大值（2）
-		out = np.max(col, axis=1)
+		out = np.max(col, axis=1)		## 采用的是max池化
 		## 转换
-		out = out.reshape(N, out_h, out_w, C).transpose(0,3,1,2)
+		out = out.reshape(N, out_h, out_w, C).transpose(0,3,1,2)	## 最后要把数据转换回原本的格式
 
 		return out
-		
+
